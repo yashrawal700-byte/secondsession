9fe92371acae5d56bd82939f5498bc64ba05a7a5
Your Name <youremail@example.com>
2025-09-24 11:52:08 +0530

    first commit

--- p.py
+++ p.py
@@ -1,2 +1,25 @@
-print("Hello world")
-print("Coding is joy")
+print("Prime numbers between 1 to 10 are: ")
+print(1,3,5,7)
+
+print("String palindrome")
+def is_palindrome():
+ x=eval(input("Enter a string to cheak:"))
+
+ if x==x[::-1]:
+   return True
+ else:
+   return False
+
+print("String palindrome:", is_palindrome)
+
+def is_anagram():
+  s1= input("Enter the first string:")
+  s2=input("Enter the seconfd string:")
+
+  s1= "".join(s1.lower().split())
+  s2= "".join(s2.lower().split())
+
+  if sorted(s1)==sorted(s2):
+    return True
+  else:
+    return False
